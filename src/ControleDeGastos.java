e737e8ba8ac85e121a71dd0a100b4539844f6a91
Controle de Gastos <rod.frnnds@gmail.com>
2023-11-15 23:35:18 -0300

    Controle de gastos

--- src/ControleDeGastos.java
+++ src/ControleDeGastos.java
@@ -16,9 +16,29 @@ public static void main(String[] args){
     
         Scanner scanner = new Scanner(System.in);
        
-        screen screen = new Screen();
+        JFrame frame = new JFrame("Controle de gastos");
+           frame.setVisible(true);
+           frame.setSize(800, 800);
+           frame.getContentPane().setBackground(Color.white);
+           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+           frame.setResizable(false);
+           frame.setLocationRelativeTo(null);
+           frame.setLayout(null);
+           JButton botãoGasto = new JButton("Gasto");
+           JButton botãoGanho = new JButton("Ganho");
+           botãoGanho.setBounds(300, 400, 100, 70);
+           botãoGasto.setBounds(400, 400, 100, 70);
+           Font font = new Font("Arial", Font.BOLD, 20);
+           botãoGanho.setFont(font);
+           botãoGasto.setFont(font);
+           botãoGanho.setBackground(Color.green);
+           botãoGasto.setBackground(Color.RED);
+           botãoGanho.setForeground(Color.BLACK);
+           botãoGasto.setForeground(Color.BLACK);
+           frame.add(botãoGasto);
+           frame.add(botãoGanho);
 
-        screen.screen();
+
 
 
 
@@ -57,6 +77,9 @@ public static void main(String[] args){
 
         }
        
+       
+    }
+       
 
 
 
@@ -69,4 +92,4 @@ public static void main(String[] args){
 
 
     
-}
+
