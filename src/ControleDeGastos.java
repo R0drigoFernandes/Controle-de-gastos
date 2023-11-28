79a66240999d0c80bebc09f37963a0dd72bd5943
Controle de Gastos <rod.frnnds@gmail.com>
2023-11-28 12:32:52 -0300

    Controle de gastos

--- src/ControleDeGastos.java
+++ src/ControleDeGastos.java
@@ -1,16 +1,4 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.Scanner;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JOptionPane;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
+
 public class ControleDeGastos{
 
 public static void main(String[] args){
--- src/screen.java
+++ src/screen.java
@@ -2,7 +2,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JOptionPane;
 import java.awt.Color;
