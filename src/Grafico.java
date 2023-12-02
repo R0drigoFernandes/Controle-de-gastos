787468a20715efc5dd6efa2f32b1e3b7446b4a01
Controle de Gastos <rod.frnnds@gmail.com>
2023-12-02 20:25:58 -0300

    Controle de gastos

--- src/Grafico.java
+++ src/Grafico.java
@@ -5,10 +5,11 @@ public class Grafico {
    public void grafico(Graphics g){
        g.setColor(Color.white);
        g.fillRect(100, 100, 100, 100);
-       g.setColor(Color.red);
-       g.fillArc(100, 100, 100, 100, 0, 360);
        g.setColor(Color.green);
-       g.fillArc(100, 100, 100, 100, 0, 340);
+       g.fillArc(100, 100, 100, 100, 0, 360);
+       g.setColor(Color.red);
+       g.fillArc(100, 100, 100, 100, 0, 20);
+      
       
        
    }
