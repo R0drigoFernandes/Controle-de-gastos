bbfa84ce80f80022809c4d4c8ea1a8ed9c9af265
Controle de Gastos <rod.frnnds@gmail.com>
2023-12-12 19:38:39 -0300

    controle de gastos

--- src/ControleDeGastos.java
+++ src/ControleDeGastos.java
@@ -1,22 +1,24 @@
+import javax.swing.SwingUtilities;
 
 public class ControleDeGastos{
 
 public static void main(String[] args){
         screen screen = new screen();
         Grafico grafico = new Grafico();
-        
+        Ganhos ganhos = new Ganhos();
+        Gasto gasto = new Gasto();
 
         while (true) { 
         
             
             screen.screen();
-            grafico.grafico(screen.frame.getGraphics());
-            
+            grafico.grafico(250, 250, 360, ganhos.getGanhos(), gasto.getGasto());
+            grafico.drawPieChart(screen.frame.getGraphics());
            
             
             
             try {
-                Thread.sleep(1000/60);
+                Thread.sleep(1000/120);
             } catch (Exception e) {
              e.printStackTrace();
  
--- src/Grafico.java
+++ src/Grafico.java
@@ -1,23 +1,40 @@
 import java.awt.Graphics;
 import java.awt.Color;
 public class Grafico {
-   public int anguloGreen, anguloRed;
-Ganhos ganho = new Ganhos();
-Gasto gasto = new Gasto();
+    int centerX = 0;
+    int centerY = 0;
+    int radius = 0;
+    double total = 0;
+    double gastosAngle = 0;
+    double economiaAngle = 0;
+    double gastos = 0;
+
+    public void grafico(int centerX, int centerY, int radius, double ganhos, double gastos) {
+        this.gastos = gastos;
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+
+         total = ganhos + gastos;
+         gastosAngle = (int) Math.round((gastos / total) * 360);
+         economiaAngle = 360 - gastosAngle;
+
+        
+    }
 
     
-   public void grafico(Graphics g){
-       
-        if(ganho.getGanhos() > 0){
-        anguloGreen = 360;
-       g.setColor(Color.green);
-       g.fillArc(100, 100, 100, 100, 0, anguloGreen);
-        }
-       if(gasto.getGasto() > 0){
-           anguloRed = (int) (gasto.getGasto() * 3.6);
-           g.setColor(Color.red);
-       g.fillArc(100, 100, 100, 100, 0, anguloRed); 
-       }
+public void drawPieChart(Graphics g) {
+    int gastosAngle = (int) Math.round((gastos / total) * 360);
+    int economiaAngle = 360 - gastosAngle;
+
+    // Define a cor dos gastos
+    g.setColor(new Color(255, 0, 0));
+    g.fillArc(centerX - radius, centerY - radius, 2 * radius, 2 * radius, 0, gastosAngle);
+
+    // Define a cor da economia
+    g.setColor(new Color(0, 255, 0));
+    g.fillArc(centerX - radius, centerY - radius, 2 * radius, 2 * radius, gastosAngle, economiaAngle);
+}
       
       
       
@@ -26,4 +43,4 @@ Gasto gasto = new Gasto();
 
    
    
-}
+
